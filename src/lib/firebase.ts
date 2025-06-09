5e7c38db5fae96f0f0da897f8615ec51ad0a8e9b
AG-lang <1746129536@qq.com>
2025-06-09 13:43:52 +0800

    Feat: Use environment variables for Firebase config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,23 +1,20 @@
-// lib/firebase.ts
+// src/lib/firebase.ts
 
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
+// 从环境变量中安全地读取配置
 const firebaseConfig = {
-    apiKey: "AIzaSyCZy59ITQEDRyHhwpsRqBIt2ZEyAhEp8qs",
-    authDomain: "password-18749.firebaseapp.com",
-    projectId: "password-18749",
-    storageBucket: "password-18749.firebasestorage.app",
-    messagingSenderId: "819084529737",
-    appId: "1:819084529737:web:a87877c88460714c0aaf98"
-  };
-  
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+};
 
-// 初始化 Firebase App
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
 
-// 导出你需要的服务
 export const auth = getAuth(app);
 export const db = getFirestore(app);
